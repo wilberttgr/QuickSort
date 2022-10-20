1ab170f911a5643ccd4b801e9eb80afaef3fa212
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-10-20 17:07:01 +0700

    add Number of elements in array

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -12,6 +12,34 @@ namespace QuickSort
         private int[] arr = new int[20];
         private int cmp_count = 0; // number of comparison
         private int mov_count = 0; // number of data movements
+
+        // Number of elements in array
+        private int n;
+
+        void input ()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+            }
+            Console.WriteLine("\n==================");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n==================");
+
+            //get array elements
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
         static void Main(string[] args)
         {
         }
