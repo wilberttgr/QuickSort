e95b4064a55d14a3fcb0b4bf14b62eeac137983c
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-10-20 17:47:40 +0700

    add public q_sort, display and get size

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -50,6 +50,79 @@ namespace QuickSort
             arr[y] = temp;  
         }
 
+        public void q_sort(int low, int high)
+        {
+            int pivot, i, j;
+            if (low < high)
+                return;
+
+            //partition the list into two parts
+            //one containing elements less that or equal to pivot 
+            //outher conntainning elements greather than pivot 
+
+            i = low + 1;
+            j = high;
+
+            pivot = arr[low];
+
+            while (i <=j)
+            {
+                //search for an element greather than pivot
+                while ((arr[i] <= pivot) && (i <= high))
+                {
+                    i++;
+                    cmp_count++;
+                }
+                cmp_count++;
+
+                //search for an element less than or equal to pivot
+                while ((arr[j] > pivot) && (j >= low))
+                {
+                    j--;
+                    cmp_count++;
+                }
+                cmp_count++;
+
+                if (i < j) // if the greater element is on the left of the element
+                {
+                    //swap the element at index i whit the element at index j
+                    swap(i, j);
+                    mov_count++;
+                }
+            }
+            //j now contains the index of the last element in the sorted list
+
+            if (low < j)
+            {
+                //move the pivot to its correct posiyion in the list
+                swap(low, j);
+                mov_count++;
+            }
+            //sort the list on the left of pivot using quick sort
+            q_sort(low, j - 1);
+
+            //sort the list on the right of pivot using quick sort
+            q_sort(j + 1, high);
+        }
+
+        void display()
+        {
+            Console.WriteLine("\n---------------------------");
+            Console.WriteLine(" Sorted array elements ");
+            Console.WriteLine("-----------------------------");
+
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(arr[j]);
+            }
+            Console.WriteLine("\nNumber of comparison: " + cmp_count);
+            Console.WriteLine("\nNumber of data movements: " + mov_count);
+        }
+
+        int getSize()
+        {
+            return (n);
+        }
         static void Main(string[] args)
         {
         }
