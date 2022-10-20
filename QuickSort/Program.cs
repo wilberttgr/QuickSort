9e5e8bcccf606fe7aac18c42a14983c0e2c3556d
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-10-20 17:14:00 +0700

    add temp for swap the element

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -40,6 +40,16 @@ namespace QuickSort
                 arr[i] = Int32.Parse(s1);
             }
         }
+        //swaps the element at index x with the element at index y
+        void swap(int x, int y)
+        {
+            int temp;
+
+            temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;  
+        }
+
         static void Main(string[] args)
         {
         }
